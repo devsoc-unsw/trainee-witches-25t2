b5ba60c900fe130d675aa62ddd0ca519b02afb7b
Vivian Dai <z5591919@ad.unsw.edu.au>
2025-09-13 10:46:17 +1000

    Commit changes before git pull

--- frontend/src/SavedRecipes/SavedRecipes.jsx
+++ frontend/src/SavedRecipes/SavedRecipes.jsx
@@ -1,9 +1,37 @@
+import React from "react";
 import './SavedRecipes.css';
+
+const recipes = [
+  { id: 1, title: "", img: ""},
+  { id: 2, title: "", img: ""},
+  { id: 3, title: "", img: ""},
+  { id: 4, title: "", img: ""},
+  { id: 5, title: "", img: ""},
+  { id: 6, title: "", img: ""},
+];
 
 const SavedRecipes = () => {
   return (
-    <div>
-      <h1>Saved Recipes</h1>
+    <div className="saved-page">
+      <h1 className="saved-title">Saved Recipes</h1>
+
+      <div className="recipes-grid">
+        {recipes.map((recipe) => (
+          <div key={recipe.id} className="recipe-card">
+            <img src={recipe.img} alt={recipe.title} className="recipe-img" />
+            <div className="recipe-info">
+              <p>{recipe.title}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* bottom navigation bar */}
+      <div className="bottom-nav">
+        <button></button>
+        <button></button>
+        <button></button>
+      </div>
     </div>
   );
 };
